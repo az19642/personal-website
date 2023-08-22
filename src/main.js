bb802eec0d9cfd305b8bb6ef50105c44c4b94278
Andy <azhang19642@gmail.com>
2023-08-22 18:37:45 -0400

    Added contact content

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import '../styles/modern-normalize.css';
 import '../styles/main.css';
-import '../styles/components/contact.css';
-import '../styles/components/projects.css';
-import '../styles/components/skills.css';
-import '../styles/components/home.css';
 import '../styles/components/header.css';
+import '../styles/components/home.css';
+import '../styles/components/skills.css';
+import '../styles/components/projects.css';
+import '../styles/components/contact.css';
 import '../styles/utils.css';
 
 import getScrollPadding from './utils/get-scroll-padding';
