2db26d8aa5bbaf3eb06dc78d18ef8cfd36150251
Andy <azhang19642@gmail.com>
2023-08-23 17:10:32 -0400

    Add mobile navbar and modify dark theme

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import '../styles/components/home.css';
 import '../styles/components/skills.css';
 import '../styles/components/projects.css';
 import '../styles/components/contact.css';
+import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
 
 import getScrollPadding from './utils/get-scroll-padding';
 import themeToggle from './utils/theme-toggle';
+import mobileNav from './utils/mobile-nav';
 
 getScrollPadding();
 themeToggle();
+mobileNav();
